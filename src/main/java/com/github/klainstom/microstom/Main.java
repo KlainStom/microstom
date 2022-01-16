18ca84522e8bdf155652370e2acad0ec6369df28
offbyone <offbyone@disroot.org>
2022-01-16 18:20:00 +0100

    Merge Server.java and Main.java

--- src/main/java/com/github/klainstom/microstom/Main.java
+++ src/main/java/com/github/klainstom/microstom/Main.java
@@ -1,6 +1,13 @@
 package com.github.klainstom.microstom;
 
+import com.github.klainstom.microstom.commands.Commands;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.extras.bungee.BungeeCordProxy;
+import net.minestom.server.extras.velocity.VelocityProxy;
 
 import java.io.File;
 import java.io.IOException;
@@ -8,27 +15,56 @@ import java.nio.file.Files;
 import java.util.Objects;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        if (args.length > 0 && args[0].equalsIgnoreCase("-v")) {
-            Versions.printVersionLines();
-            System.exit(0);
-        }
+    private static final String START_SCRIPT_FILENAME = "start.sh";
 
-        String filename = "start.sh";
-        File file = new File(filename);
-        if (file.isDirectory()) {
-            MinecraftServer.LOGGER.warn("Can't create startup script!");
-        }
-        if (!file.isFile()) {
+    public static void main(String[] args) throws IOException {
+        Versions.printVersionLines();
+        if (args.length > 0 && args[0].equalsIgnoreCase("-v")) System.exit(0);
+
+        File startScriptFile = new File(START_SCRIPT_FILENAME);
+        if (startScriptFile.isDirectory()) MinecraftServer.LOGGER.warn("Can't create startup script!");
+        if (!startScriptFile.isFile()) {
             MinecraftServer.LOGGER.info("Create startup script.");
             Files.copy(
-                    Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(filename)),
-                    file.toPath());
+                    Objects.requireNonNull(Main.class.getClassLoader().getResourceAsStream(START_SCRIPT_FILENAME)),
+                    startScriptFile.toPath());
             Runtime.getRuntime().exec("chmod u+x start.sh");
             System.out.println("Use './start.sh' to start the server.");
             System.exit(0);
         }
 
-        Server.main(args);
+        // Actually start server
+        Settings.read();
+
+        MinecraftServer server = MinecraftServer.init();
+
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerLoginEvent.class, event -> {
+            if (MinecraftServer.getInstanceManager().getInstances().isEmpty())
+                event.getPlayer().kick(Component.text("There is no instance available!", NamedTextColor.RED));
+        });
+
+        MinecraftServer.getCommandManager().register(Commands.SHUTDOWN);
+        MinecraftServer.getCommandManager().register(Commands.RESTART);
+
+        switch (Settings.getMode()) {
+            case OFFLINE:
+                break;
+            case ONLINE:
+                MojangAuth.init();
+                break;
+            case BUNGEECORD:
+                BungeeCordProxy.enable();
+                break;
+            case VELOCITY:
+                if (!Settings.hasVelocitySecret())
+                    throw new IllegalArgumentException("The velocity secret is mandatory.");
+                VelocityProxy.enable(Settings.getVelocitySecret());
+        }
+
+        server.start(Settings.getServerIp(), Settings.getServerPort());
+        MinecraftServer.LOGGER.info("==== SETTINGS ====");
+        MinecraftServer.LOGGER.info("Mode: {}", Settings.getMode());
+        MinecraftServer.LOGGER.info("Address: {}:{}", Settings.getServerIp(), Settings.getServerPort());
+        MinecraftServer.LOGGER.info("==================");
     }
 }
