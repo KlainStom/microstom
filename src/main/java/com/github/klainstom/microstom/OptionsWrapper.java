1a44ee6fede424b79959c2dd05d597730f59c459
offbyone <offbyone@disroot.org>
2022-01-21 15:49:03 +0100

    Fix NPE in OptionsWrapper

--- src/main/java/com/github/klainstom/microstom/OptionsWrapper.java
+++ src/main/java/com/github/klainstom/microstom/OptionsWrapper.java
@@ -11,7 +11,7 @@ public class OptionsWrapper {
             System.setProperty("minestom.chunk-view-distance", Settings.getChunkViewDistance());
         if (Settings.getEntityViewDistance() != null)
             System.setProperty("minestom.entity-view-distance", Settings.getEntityViewDistance());
-        if (Settings.getTerminalDisabled().equalsIgnoreCase("true"))
+        if ("true".equalsIgnoreCase(Settings.getTerminalDisabled()))
             System.setProperty("minestom.terminal.disabled", "");
         // TODO: 21.01.22 maybe add command line arguments to args
         Server.main(args);
