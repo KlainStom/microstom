ed56d98ea58577579075af06f86e7f35f78909e2
offbyone <offbyone@disroot.org>
2022-01-26 17:33:16 +0100

    Use terminal completion and highlighting; Skip empty lines

--- src/main/java/com/github/klainstom/microstom/terminal/MicrostomTerminal.java
+++ src/main/java/com/github/klainstom/microstom/terminal/MicrostomTerminal.java
@@ -1,43 +1,54 @@
 package com.github.klainstom.microstom.terminal;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.CommandManager;
 import net.minestom.server.command.builder.CommandResult;
 import org.jetbrains.annotations.ApiStatus;
-import org.jline.reader.EndOfFileException;
-import org.jline.reader.LineReader;
-import org.jline.reader.LineReaderBuilder;
-import org.jline.reader.UserInterruptException;
+
+import org.jline.reader.*;
+import org.jline.reader.impl.DefaultExpander;
+import org.jline.reader.impl.history.DefaultHistory;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
 
 import java.io.IOException;
 
 public class MicrostomTerminal {
+    private static CommandManager COMMAND_MANAGER;
     private static final String PROMPT = "> ";
 
     private static volatile Terminal terminal;
-    private static volatile LineReader reader;
+    private static volatile LineReader lineReader;
+    private static final Highlighter highlighter = new MicrostomHighlighter();
+    private static final Completer completer = new MicrostomCompleter();
+    private static final Expander expander = new DefaultExpander();
+    private static final History history = new DefaultHistory();
     private static volatile boolean running = false;
 
     @ApiStatus.Internal
     public static void start() {
+        COMMAND_MANAGER = MinecraftServer.getCommandManager();
         final Thread thread = new Thread(null, () -> {
             try {
                 terminal = TerminalBuilder.terminal();
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            reader = LineReaderBuilder.builder()
+            lineReader = LineReaderBuilder.builder()
                     .terminal(terminal)
+                    .highlighter(highlighter)
+                    .completer(completer)
+                    .expander(expander)
+                    .history(history)
                     .build();
             running = true;
 
             while (running) {
                 String command;
                 try {
-                    command = reader.readLine(PROMPT);
-                    var commandManager = MinecraftServer.getCommandManager();
-                    CommandResult result = commandManager.execute(commandManager.getConsoleSender(), command);
+                    command = lineReader.readLine(PROMPT);
+                    if (command.isBlank()) continue;
+                    CommandResult result = COMMAND_MANAGER.execute(COMMAND_MANAGER.getConsoleSender(), command);
                     switch (result.getType()) {
                         case UNKNOWN -> print("Unknown command.");
                         case INVALID_SYNTAX -> print("Invalid syntax: " + result.getInput());
@@ -64,10 +75,11 @@ public class MicrostomTerminal {
                 e.printStackTrace();
             }
         }
+
     }
 
     public static void print(String line) {
-        reader.printAbove(line);
+        lineReader.printAbove(line);
     }
 
     public static boolean isRunning() { return running; }
