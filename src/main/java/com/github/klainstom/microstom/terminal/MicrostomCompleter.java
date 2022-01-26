6c13fff9f6daed705b56b1a8f60dbf508e9968d7
offbyone <offbyone@disroot.org>
2022-01-26 17:28:34 +0100

    Add MicrostomCompleter

---
+++ src/main/java/com/github/klainstom/microstom/terminal/MicrostomCompleter.java
@@ -0,0 +1,30 @@
+package com.github.klainstom.microstom.terminal;
+
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.builder.Command;
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class MicrostomCompleter implements Completer {
+    @Override
+    public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
+        MinecraftServer.LOGGER.info("Word index {}", line.wordIndex());
+
+        if (line.wordIndex() == 0) {
+            Set<Command> commands = MinecraftServer.getCommandManager().getDispatcher().getCommands();
+            for (Command command : commands) {
+                candidates.addAll(Arrays.stream(command.getNames()).map(Candidate::new).toList());
+            }
+        } else {
+            Command command = MinecraftServer.getCommandManager().getCommand(line.words().get(0));
+            if (command == null) return;
+            candidates.addAll(command.getSyntaxesStrings().stream().map(Candidate::new).toList());
+        }
+    }
+}
